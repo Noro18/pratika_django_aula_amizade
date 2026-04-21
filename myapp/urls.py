85632075e948f2c46b380ed337ee6d5cbaec0cf6
Jessua Rodrigues <155636643+pedrix-48@users.noreply.github.com>
2026-04-21 16:05:09 +0900

    Organize URL patterns with comments for clarity

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,18 +2,21 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
+    # page routing
     path("", views.home, name="home"),
     path("konaba", views.konaba, name="konaba"),
-    
+
+    # CRUD
     path("lista-estudante", views.lista_estudante, name="lista"),
     path("reg/estudante", views.add_estudante, name="add"),
     path("del/estudante/<uuid:id>", views.delete_estudante, name="delete"),
     path("edit/estudante/<uuid:id>", views.edit_estudante, name="edit"),
-    
+
+    # filter
     path("lista-dep", views.listadep, name="listadep"),
     path("lista/dep/estudante/<uuid:id>", views.estudante_dep, name="estudante-dep"),
     
-    
+    # auth
     path("loginpage", views.loginpage, name="loginpage"),
     path("logout", views.logout_user, name="logout"),
     
